a03c83a9a1af17ee8f309a90162c6f6790d7aaa5
jhermo <jbhermo@yahoo.com>
2022-10-19 16:04:25 -0400

    Initial commit with home page

---
+++ app/src/main/java/com/example/mealplanner/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.mealplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FrameLayout homeFrame;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Created a fragment which will be used as home page
+        homeFrame = findViewById(R.id.homeFrame);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.homeFrame, new HomeFragment());
+        transaction.commit();
+    }
+}
