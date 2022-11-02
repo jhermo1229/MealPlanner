d655f96a1acec6f43d804f4ca1d8264e374e5d69
jhermo <jbhermo@yahoo.com>
2022-11-02 16:59:23 -0400

    Updated settings for error encountered Build was configured to prefer settings repositories over project repositories but repository 'maven' was added by build file 'build.gradle'

--- app/src/main/java/com/example/mealplanner/MainActivity.java
+++ app/src/main/java/com/example/mealplanner/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.mealplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
 
         //Created a fragment which will be used as home page
         homeFrame = findViewById(R.id.homeFrame);
