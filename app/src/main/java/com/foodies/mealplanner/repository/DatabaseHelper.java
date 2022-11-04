4500e79f13c26336b20b4fe1cbee457711140f2a
jhermo <jbhermo@yahoo.com>
2022-11-04 09:21:25 -0400

    Added repo for firebase

--- app/src/main/java/com/foodies/mealplanner/repository/DatabaseHelper.java
+++ app/src/main/java/com/foodies/mealplanner/repository/DatabaseHelper.java
@@ -1,8 +1,59 @@
 package com.foodies.mealplanner.repository;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+
+import com.foodies.mealplanner.model.User;
+import com.foodies.mealplanner.ui.MainActivity;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 /**
  * Database handler for meal planner.
  * Uses firebase -> cloud firestore
  */
 public class DatabaseHelper {
+
+    public static final String USER_CREDENTIALS = "userCredentials";
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    public void addCustomerUser(User user, Activity activity) {
+
+        CollectionReference dbCourses = db.collection(USER_CREDENTIALS);
+        dbCourses.add(user).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(@NonNull @org.jetbrains.annotations.NotNull DocumentReference documentReference) {
+
+                Log.i("Database", "Successful adding user");
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+                final EditText text = new EditText(activity);
+                builder.setTitle("Meal Planner").setMessage("Successfully Created Meal Order").setView(text);
+                builder.setPositiveButton("Continue", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface di, int ii) {
+                        Intent i = new Intent(activity, MainActivity.class);
+                        // i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // Call Only, if you wants to clears the activity stack else ignore it.
+                        activity.startActivity(i);
+                        activity.finish();
+                    }
+                });
+                builder.create().show();
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e("Database", "Unsuccessful adding user" + e.toString());
+            }
+        });
+    }
 }
--- app/src/main/java/com/foodies/mealplanner/ui/PaymentDetailsFragment.java
+++ app/src/main/java/com/foodies/mealplanner/ui/PaymentDetailsFragment.java
@@ -4,27 +4,23 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.foodies.mealplanner.R;
-import com.foodies.mealplanner.viewmodel.SharedViewModel;
 import com.foodies.mealplanner.model.User;
 import com.foodies.mealplanner.model.UserPaymentDetails;
+import com.foodies.mealplanner.repository.DatabaseHelper;
 import com.foodies.mealplanner.util.AppUtils;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.foodies.mealplanner.viewmodel.SharedViewModel;
 import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
 
 /**
  * Fragment class for payment details of user
@@ -32,25 +28,18 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class PaymentDetailsFragment extends Fragment {
 
     public static final String TAG = PaymentDetailsFragment.class.getName();
+    private final DatabaseHelper db = new DatabaseHelper();
+    private final AppUtils appUtils = new AppUtils();
+    private final UserPaymentDetails userPaymentDetails = new UserPaymentDetails();
+    private final User user = new User();
     private SharedViewModel sharedViewModel;
-    private FirebaseFirestore db;
     private View paymentDetailsView;
-    private AppUtils appUtils = new AppUtils();
     private TextInputLayout nameOnCard, cardNumber, expiryDate, securityCode;
-    private UserPaymentDetails userPaymentDetails = new UserPaymentDetails();
     private Button saveButton;
-
 
 
     public PaymentDetailsFragment() {
         // Required empty public constructor
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        db = FirebaseFirestore.getInstance();
-
     }
 
     @Override
@@ -63,68 +52,24 @@ public class PaymentDetailsFragment extends Fragment {
         securityCode = paymentDetailsView.findViewById(R.id.securityCode);
         saveButton = paymentDetailsView.findViewById(R.id.saveButton);
 
+        saveButton.setOnClickListener((personalDetailView) -> {
 
-    saveButton.setOnClickListener((personalDetailView) -> {
+            userPaymentDetails.setNameOnCard(nameOnCard.getEditText().getText().toString());
+            userPaymentDetails.setCardNumber(Double.valueOf(cardNumber.getEditText().getText().toString()));
+            userPaymentDetails.setExpiryDate(Integer.valueOf(expiryDate.getEditText().getText().toString()));
+            String securityCodeEncrypt = appUtils.encodeBase64(securityCode.getEditText().getText().toString());
+            userPaymentDetails.setSecurityCode(securityCodeEncrypt);
 
+            sharedViewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
+            user.setEmail(sharedViewModel.getSelectedItem().getValue().getEmail());
+            user.setPassword(sharedViewModel.getSelectedItem().getValue().getPassword());
+            user.setUserDetails(sharedViewModel.getSelectedItem().getValue().getUserDetails());
+            user.setUserMealDetails(sharedViewModel.getSelectedItem().getValue().getUserMealDetails());
+            user.setUserPaymentDetails(userPaymentDetails);
 
-        userPaymentDetails.setNameOnCard(nameOnCard.getEditText().getText().toString());
-        userPaymentDetails.setCardNumber(Double.valueOf(cardNumber.getEditText().getText().toString()));
-        userPaymentDetails.setExpiryDate(Integer.valueOf(expiryDate.getEditText().getText().toString()));
-        String securityCodeEncrypt = appUtils.encodeBase64(securityCode.getEditText().getText().toString());
-        userPaymentDetails.setSecurityCode(securityCodeEncrypt);
-        addDataToFirestore();
-
-
-    });
-
+            db.addCustomerUser(user, getActivity());
+        });
 
         return paymentDetailsView;
-    }
-
-
-    private void addDataToFirestore() {
-
-        // creating a collection reference
-        // for our Firebase Firestore database.
-
-        User user = new User();
-        CollectionReference dbCourses = db.collection("userCredentials");
-        sharedViewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
-        user.setEmail(sharedViewModel.getSelectedItem().getValue().getEmail());
-        user.setPassword(sharedViewModel.getSelectedItem().getValue().getPassword());
-        user.setUserDetails(sharedViewModel.getSelectedItem().getValue().getUserDetails());
-        user.setUserMealDetails(sharedViewModel.getSelectedItem().getValue().getUserMealDetails());
-        user.setUserPaymentDetails(userPaymentDetails);
-
-        // below method is use to add data to Firebase Firestore.
-        dbCourses.add(user).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-            @Override
-            public void onSuccess(DocumentReference documentReference) {
-                // after the data addition is successful
-                // we are displaying a success toast message.
-                //Toast.makeText(getContext(), "Your Course has been added to Firebase Firestore", Toast.LENGTH_SHORT).show();
-                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-                final EditText text = new EditText(getActivity());
-                builder.setTitle("Meal Planner").setMessage("Successfully Created Meal Order").setView(text);
-                builder.setPositiveButton("Continue", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface di, int ii) {
-                        Intent i = new Intent(getActivity(), MainActivity.class);
-                        // i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // Call Only, if you wants to clears the activity stack else ignore it.
-                        startActivity(i);
-                        getActivity().finish();
-                    }  });
-                builder.create().show();
-
-
-
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                // this method is called when the data addition process is failed.
-                // displaying a toast message when data addition is failed.
-                //Toast.makeText(getContext(), "Fail to add course \n" + e, Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
