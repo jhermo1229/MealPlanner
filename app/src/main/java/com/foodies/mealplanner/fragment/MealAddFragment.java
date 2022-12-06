d7bc4a9295cf5f010bd465b6228b8414bbcad29e
jhermo <jbhermo@yahoo.com>
2022-12-06 12:01:41 -0500

    Added Image Adder in Create new meal

--- app/src/main/java/com/foodies/mealplanner/fragment/MealAddFragment.java
+++ app/src/main/java/com/foodies/mealplanner/fragment/MealAddFragment.java
@@ -1,7 +1,13 @@
 package com.foodies.mealplanner.fragment;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -12,14 +18,22 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.foodies.mealplanner.R;
 import com.foodies.mealplanner.model.Meal;
 import com.foodies.mealplanner.repository.MealRepository;
 import com.foodies.mealplanner.validations.FieldValidator;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
-import java.math.BigDecimal;
 import java.text.NumberFormat;
 
 public class MealAddFragment extends Fragment {
@@ -30,13 +44,47 @@ public class MealAddFragment extends Fragment {
     private Spinner mealTypeSpinner;
     private Meal meal = new Meal();
     private String[] mealTypeList;
-    private Button okButton, cancelButton;
-    private MealRepository db = new MealRepository();
+    private Button okButton, cancelButton, mealImageAddButton;
+    private MealRepository mealRepository = new MealRepository();
     private static final String REQUIRED_ERROR = "Required";
     private final FieldValidator fieldValidator = new FieldValidator();
+    private ImageView imageView;
+    private ActivityResultLauncher<Intent> launchSomeActivity;
+    // instance for firebase storage and StorageReference
+    private final FirebaseStorage storage = FirebaseStorage.getInstance();
+    private final StorageReference storageReference = storage.getReference();
 
     public static MealAddFragment newInstance() {
         return new MealAddFragment();
+    }
+
+    /**
+     * On create, set activity for image.
+     *
+     * @param savedInstanceState - current instance of fragment
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        launchSomeActivity
+                = registerForActivityResult(
+                new ActivityResultContracts
+                        .StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode()
+                            == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        // do your operation from here....
+                        if (data != null
+                                && data.getData() != null) {
+                            Uri selectedImageUri = data.getData();
+
+                            uploadImage(selectedImageUri);
+
+                        }
+                    }
+                });
     }
 
     @Override
@@ -56,6 +104,12 @@ public class MealAddFragment extends Fragment {
         mealTypeSpinner.setAdapter(adapterMealType);
         okButton = mealProfileFragmentView.findViewById(R.id.okButtonMealAdd);
         cancelButton = mealProfileFragmentView.findViewById(R.id.cancelButtonMealAdd);
+        mealImageAddButton = mealProfileFragmentView.findViewById(R.id.mealAddImageButton);
+        imageView = mealProfileFragmentView.findViewById(R.id.mealImageAdd);
+
+        mealImageAddButton.setOnClickListener((mealProfileFragmentView) -> {
+            imageChooser();
+        });
 
         okButton.setOnClickListener((mealProfileFragmentView)->{
 
@@ -72,7 +126,9 @@ public class MealAddFragment extends Fragment {
                 meal.setMealPrice(format.format(Double.valueOf(mealPriceTxt.getText().toString())));
                 meal.setMealStatus(ACTIVE);
 
-                db.addMeal(meal, getActivity());
+
+
+                mealRepository.addMeal(meal, getActivity());
                 getParentFragmentManager().popBackStackImmediate();
             }
         });
@@ -127,6 +183,115 @@ public class MealAddFragment extends Fragment {
         mealDescriptionTxt.setError(null);
         mealIngredientTxt.setError(null);
         mealPriceTxt.setError(null);
+    }
+
+    /**
+     * uploads the image in the cloud.
+     *
+     * @param filePath - current location of the file in the device.
+     */
+    private void uploadImage(Uri filePath) {
+        if (filePath != null) {
+
+            // Code for showing progressDialog while uploading
+            ProgressDialog progressDialog
+                    = new ProgressDialog(getContext());
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            // Defining the child of storageReference of meals in the cloud.
+            StorageReference ref
+                    = storageReference
+                    .child(
+                            "images/meals/"
+                                    + meal.getMealName());
+
+            // adding listeners on upload
+            // or failure of image
+            ref.putFile(filePath)
+                    .addOnSuccessListener(
+                            new OnSuccessListener<UploadTask.TaskSnapshot>() {
+
+                                @Override
+                                public void onSuccess(
+                                        UploadTask.TaskSnapshot taskSnapshot) {
+                                    ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                        @Override
+                                        public void onSuccess(Uri uri) {
+                                            // Image uploaded successfully
+                                            meal.setImageUrl(uri.toString());
+
+                                            //updates database on location of the image. We used access token on cloud.
+//                                            mealRepository.updateMeal(meal, getActivity());
+
+                                            //loads image in the view
+                                            loadImage(uri);
+                                        }
+                                    });
+
+                                    // Dismiss dialog
+                                    progressDialog.dismiss();
+
+                                    Toast
+                                            .makeText(getContext(),
+                                                    "Image Uploaded!!",
+                                                    Toast.LENGTH_SHORT)
+                                            .show();
+                                }
+                            })
+
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                            // Error, Image not uploaded
+                            progressDialog.dismiss();
+                            Toast
+                                    .makeText(getContext(),
+                                            "Failed " + e.getMessage(),
+                                            Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+                    })
+                    .addOnProgressListener(
+                            new OnProgressListener<UploadTask.TaskSnapshot>() {
+
+                                // Progress Listener for loading
+                                // percentage on the dialog box
+                                @Override
+                                public void onProgress(
+                                        UploadTask.TaskSnapshot taskSnapshot) {
+                                    double progress
+                                            = (100.0
+                                            * taskSnapshot.getBytesTransferred()
+                                            / taskSnapshot.getTotalByteCount());
+                                    progressDialog.setMessage(
+                                            "Uploaded "
+                                                    + (int) progress + "%");
+                                }
+                            });
+        }
+    }
+
+    /**
+     * Open the images folder in the root of the device.
+     */
+    private void imageChooser() {
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        launchSomeActivity.launch(i);
+    }
+
+    /**
+     * Method for loading image using url
+     */
+    private void loadImage(Uri uri) {
+
+        //open source Picasso
+        Picasso.get().load(uri)
+                .into(imageView);
     }
 
 }
